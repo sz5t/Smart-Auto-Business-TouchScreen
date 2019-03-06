508fa240482efe27d773b5bb31a2e45e8b5d5d7d
zxlgrandia <grandia-4727@163.com>
2019-03-06 16:04:25 +0800

    update

--- src/assets/webConfig.js
+++ src/assets/webConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
     /** web服务  url: "http://192.168.1.111:8083"  http://localhost:4200*/
-     localResourceUrl: "http://localhost:8083",
+     localResourceUrl: "http://192.168.1.2:8084",
      /** 后台服务 */
-     settingSystemServer: 'http://localhost:8081/api.cfg/',
+     settingSystemServer: 'http://192.168.1.2:8081/api.cfg/',
     /** 后台服务 */
-     appSystemServer: 'http://localhost:8081/api.cfg/',
+     appSystemServer: 'http://192.168.1.2:8081/api.cfg/',
     /**报表  */
-     reportServerUrl: 'http://localhost:8088/ReportServer.ashx'
+     reportServerUrl: 'http://192.168.1.2:8088/ReportServer.ashx'
 }
